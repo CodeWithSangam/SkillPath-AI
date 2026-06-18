17b87b417fde9d5fba499a5ffcabf988c80f1263
CodeWithSangam <gsangam781@gmail.com>
2026-06-18 14:03:46 +0530

    SkillPath AI

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  let name = 'Sangam'
+  return (
+    <>
+      <h1>Hi Everyone, I'm {name}</h1>
+      <p>I'm Learning Full Stack Development</p>
+    </>
+  )
+}
+
+export default App
