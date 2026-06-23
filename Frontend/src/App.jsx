4fb2e34b8759c52b5bb0a9701a4ed95e1ba6754a
CodeWithSangam <gsangam781@gmail.com>
2026-06-23 14:07:39 +0530

    Done some changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,17 @@
-import Navbar from "./components/layout/Navbar"
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './components/layout/Home';
+import About from './components/layout/About';
+import Contact from './components/layout/Contact';
 
 function App() {
   return(
-    <>
-    <h1>SkillPath AI</h1>
-    <Navbar/>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/contact" element={<Contact/>} />
+    </Routes>
+    </BrowserRouter>
   )
   
 }
