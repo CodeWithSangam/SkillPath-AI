f8f786636165627a934180efd7b8cfd8747cc63d
CodeWithSangam <gsangam781@gmail.com>
2026-06-23 12:43:03 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,13 @@
+import Greeting from "./components/Greeting"
+
 function App() {
   let name = 'Sangam'
   return (
     <>
-      <h1>Hi Everyone, I'm {name}</h1>
-      <p>I'm Learning Full Stack Development</p>
+      <h1 className="heading" style={{backgroundColor:"red",color:"white"}}>Hi Everyone, I'm {name}</h1>
+      <p>I'm Learning Full Stack Share Market</p>
+      <Greeting/>
+      <Greeting/>
     </>
   )
 }
