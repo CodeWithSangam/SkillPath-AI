5b2efd3c841f379fa07ef76b31468ef2971a0f4a
CodeWithSangam <gsangam781@gmail.com>
2026-06-23 12:59:14 +0530

    Done some changes

--- Frontend/src/components/Greeting.jsx
+++ Frontend/src/components/Greeting.jsx
@@ -1,7 +1,12 @@
+import Card from "./Card";
+
 function Greeting() {
     let num = 93;
     return(
+        <>
         <h1>Namaste, Modi Ji Kya hoga ab {num}</h1>
+        <Card/>
+        </>
     )
 }
 export default Greeting
