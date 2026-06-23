0375bfcff2acff10d4e163cfa4f9744c01d95783
CodeWithSangam <gsangam781@gmail.com>
2026-06-23 12:59:03 +0530

    Done some changes

---
+++ Frontend/src/components/Card.jsx
@@ -0,0 +1,10 @@
+function Card(){
+    return(
+        <div>
+            <h1>Shoes</h1>
+            <img height={200} width={400} src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQUMyGRmSWPJSskvI3gKkIozfAXMoV3pwP_jNfttHDyWXRoZ02sEjT7tAeVFTpyNZVkaeHMQI-3eWnDYgqZ6ckVIH7yLmy8IoAiteh3QuXggbcilGqqB_FVOw" alt="" />
+            <p>Price: 20$</p>
+        </div>
+    )
+}
+export default Card
