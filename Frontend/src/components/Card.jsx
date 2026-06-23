17d3b4960cb3f149bbd7cb5ab4310ee56c0200e8
CodeWithSangam <gsangam781@gmail.com>
2026-06-23 13:24:20 +0530

    Done some changes

--- Frontend/src/components/Card.jsx
+++ Frontend/src/components/Card.jsx
@@ -2,8 +2,8 @@ function Card(props){
     return(
         <div>
             <h1>Name:{props.name}</h1>
-            <img height={200} width={400} src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQUMyGRmSWPJSskvI3gKkIozfAXMoV3pwP_jNfttHDyWXRoZ02sEjT7tAeVFTpyNZVkaeHMQI-3eWnDYgqZ6ckVIH7yLmy8IoAiteh3QuXggbcilGqqB_FVOw" alt="" />
-            <p>Age:{props.age}</p>
+            <img height={250} width={400} src={props.img} alt="" />
+            <h1>Age:{props.age}</h1>
         </div>
     )
 }
