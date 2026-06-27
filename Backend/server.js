4f4707c3655f6910feed84081889c229b55614e1
CodeWithSangam <gsangam781@gmail.com>
2026-06-27 12:05:25 +0530

    init

---
+++ Backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 5000;
+
+app.use(express.json());
+
+app.get('/',(req, res) => {
+    res.send("Backend Server Running Successfully...")
+});
+
+app.listen(PORT, () => {
+    console.log((`Server is running on port: ${PORT}`));
+})
